a5b9b9cf6c3f63eff5f4835b846e91d4fc97c88e
Zero <nhthang312@gmail.com>
2025-10-30 16:32:52 +0700

    feat: add news & academic frontend services

---
+++ src/services/index.ts
@@ -0,0 +1,16 @@
+// Export all API services
+export * from './auth';
+export * from './courses';
+export * from './student';
+export * from './admin';
+export { studentsApi, teachersApi } from './academic';
+export type { Teacher } from './academic';
+export * from './ticketService';
+export * from './news';
+
+// Export new services
+export * from './classes';
+export * from './attendance';
+export * from './assignments';
+export * from './materials';
+export * from './surveys';
